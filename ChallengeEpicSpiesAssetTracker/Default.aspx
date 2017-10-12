1406e72e32b350b08c8b7b77dc93e916b7b81ae5
Suhaib Al-tamimi <suahib.altimimi@gmail.com>
2017-10-12 12:52:28 -0700

    Add project files.

---
+++ ChallengeEpicSpiesAssetTracker/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssetTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="Image1" runat="server" Height="191px" ImageUrl="~/epic-spies-logo.jpg" Width="173px" />
+            <br />
+            <h1 class="auto-style2">Asset Performance Tracker</h1>
+            <p class="auto-style2">
+                Asset Name: <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p class="auto-style2">
+                Elections Rigged:
+                <asp:TextBox ID="riggedTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p class="auto-style2">
+                Acts of Subterfuge Performed:
+                <asp:TextBox ID="subterfugeTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p class="auto-style2">
+                &nbsp;</p>
+            <p class="auto-style2">
+                <asp:Button ID="addAssetBtn" runat="server" OnClick="addAssetBtn_Click" Text="Add Asset" />
+            </p>
+            <p class="auto-style2">
+                &nbsp;</p>
+            <p class="auto-style2">
+                <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssetTracker/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssetTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assetName = new string[0];
+                int[] rigged = new int[0];
+                int[] subterfuge = new int[0];
+
+                ViewState.Add("AssetName", assetName);
+                ViewState.Add("ElectionsRigged", rigged);
+                ViewState.Add("SubterfugePerformed", subterfuge);
+            }
+
+        }
+
+        protected void addAssetBtn_Click(object sender, EventArgs e)
+        {
+            string[] assetName = (string[])ViewState["AssetName"];
+            int assetLength = assetName.Length + 1;
+            Array.Resize(ref assetName, assetLength);
+            int assetIndex = assetName.GetUpperBound(0);
+
+            int[] rigged = (int[])ViewState["ElectionsRigged"];
+            int riggedLength = rigged.Length + 1;
+            Array.Resize(ref rigged, riggedLength);
+            int riggedIndex = rigged.GetUpperBound(0);
+
+            int[] subterfuge = (int[])ViewState["SubterfugePerformed"];
+            int subterfugeLength = subterfuge.Length + 1;
+            Array.Resize(ref subterfuge, subterfugeLength);
+            int subterfugeIndex = subterfuge.GetUpperBound(0);
+
+            assetName[assetIndex] = nameTextBox.Text;
+            rigged[riggedIndex] = int.Parse(riggedTextBox.Text);
+            subterfuge[subterfugeIndex] = int.Parse(subterfugeTextBox.Text);
+
+            ViewState["AssetName"] = assetName;
+            ViewState["ElectionsRigged"] = rigged;
+            ViewState["SubterfugePerformed"] = subterfuge;
+
+            resultLabel.Text = String.Format("Total Elections Rigged: {0}" +
+                "<br /> Average Acts of Subterfuge per Asset: {1:N2}" +
+                "<br />(Last Asset you Added: {2})", rigged.Sum(), subterfuge.Average(),assetName.Last());
+
+            nameTextBox.Text = "";
+            riggedTextBox.Text = "";
+            subterfugeTextBox.Text = "";
+
+
+
+
+        }
+    }
+}
